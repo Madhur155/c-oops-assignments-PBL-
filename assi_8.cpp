eefcfcff1aef819ac6bbf378348505dbe9cf2635
madhur <burghatemadhur@gmail.com>
2022-12-01 21:52:05 +0530

    try

--- assi_8.cpp
+++ assi_8.cpp
@@ -1,6 +1,3 @@
 // Write a program to maintain an employee database in binary file with employee information such as empId,
 // name, age, department, post and salary. Write function for adding new record, displaying all records,
 // searching for a particular employee, updating employee salary and post.
-// jgigudf
-// awrgwergtewr
-// knuegn
